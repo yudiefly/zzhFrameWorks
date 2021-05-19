c245863200253bf49725f571143320d2984a9190
yudiefly <zzh203@126.com>
2021-05-19 16:03:53 +0800

    upgrades for gfs

---
+++ Samples/ExperimentalPark/MongoDbGFSDemo/myMongoToolTest/FrmMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace myMongoToolTest
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件:";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(63, 27);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(719, 25);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "*.*|*.*";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(788, 25);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(51, 27);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "…";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(63, 58);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(175, 53);
+            this.btnUpload.TabIndex = 3;
+            this.btnUpload.Text = "文件上传到MongoDB";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 595);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "myTools Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ Samples/ExperimentalPark/MongoDbGFSDemo/myMongoToolTest/FrmMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZZH.MongoDB.Service;
+using System.IO;
+
+namespace myMongoToolTest
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFileName.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (this.txtFileName.Text.Trim() != "")
+            {
+                if (File.Exists(this.txtFileName.Text))
+                {
+                    var config = new MongoConfig
+                    {
+                        AuthDb = "admin",
+                        ConnectTimeout = 300000,
+                        DefaultDb = "yudiefly",
+                        PassWord = "zzh203@126.com",
+                        UserName = "yudiefly",
+                        ServerPort = 27017,
+                        ServerConStr = "139.224.196.218",
+                        SocketTimeout = 180000,
+                        MaxConnectionIdleTime = 60000,
+                        MaxConnectionLifeTime = 600000,
+                        MaxConnectionPoolSize = 600,
+                        WaitQueueSize = 10,
+                        WaitQueueTimeout = 120000
+                    };
+                    var repostory = new DoMongoRepostory<myValueObject>("yudiefly", "test_file_upload", config);
+                    var result = repostory.UploadFile(this.txtFileName.Text, "test_file_upload");
+                   
+                }
+            }
+
+
+
+
+        }
+    }
+    public class myValueObject : AggregateBase
+    {
+
+    }
+}
